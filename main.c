0864777973a6e8d5359bda5113c69e083c73718f
Mohamed-Amr-Shalaby <136722563+Mohamed-Amr-Shalaby@users.noreply.github.com>
2023-12-20 01:17:17 +0200

    Added comments for better readability

--- main.c
+++ main.c
@@ -4,57 +4,84 @@
 #define MAX_DOCUMENT_LENGTH 100
 #define MAX_QUEUE_SIZE 10
 
-typedef struct PrintJob {
+// define the PrintJob structure here
+typedef struct PrintJob 
+{
     char document[MAX_DOCUMENT_LENGTH];
     struct PrintJob *next;
 } PrintJob;
 
-typedef struct {
+// define the PrinterQueue structure here
+typedef struct 
+{
     PrintJob *front, *rear;
     int count;
 } PrinterQueue;
 
-void initializePrinterQueue(PrinterQueue *queue) {
+// Initialize the printer queue to an empty state
+void initializePrinterQueue(PrinterQueue *queue) 
+{
     queue->front = queue->rear = NULL;
     queue->count = 0;
 }
 
-int isEmpty(PrinterQueue *queue) {
+// Check if the printer queue is empty
+int isEmpty(PrinterQueue *queue) 
+{
     return queue->count == 0;
 }
 
-int isFull(PrinterQueue *queue) {
+// Check if the printer queue is full
+int isFull(PrinterQueue *queue) 
+{
     return queue->count == MAX_QUEUE_SIZE;
 }
 
-void submitPrintJob(PrinterQueue *queue, char *document) {
-    if (!isFull(queue)) {
+// Add a new print job to the printer queue
+void submitPrintJob(PrinterQueue *queue, char *document) 
+{
+    // Check if the queue is full
+    if (!isFull(queue)) 
+    {
         PrintJob *newJob = (PrintJob *)malloc(sizeof(PrintJob));
-        if (newJob == NULL) {
+        // Check if memory allocation failed
+        if (newJob == NULL) 
+        {
             printf("Memory allocation failed. Cannot submit print job.\n");
             return;
         }
 
-        snprintf(newJob->document, MAX_DOCUMENT_LENGTH, "%s", document);
+        // Copy the document name into the new print job
+        strcpy(newJob->document, document);
         newJob->next = NULL;
 
-        if (isEmpty(queue)) {
+        // Add the new print job to the queue
+        // If the queue is empty, the new job is both the front and rear
+        if (isEmpty(queue)) 
+        {
             queue->front = queue->rear = newJob;
-        } else {
+        }
+        // Otherwise, add the new job to the rear of the queue 
+        else 
+        {
             queue->rear->next = newJob;
             queue->rear = newJob;
         }
-
+        // Increment the number of print jobs in the queue
         queue->count++;
 
         printf("Print job submitted to the printer queue: %s\n", document);
-    } else {
+    } 
+    else 
+    {
         printf("The printer queue is full. Cannot submit more print jobs!\n");
     }
 }
 
-void processPrintJob(PrinterQueue *queue) {
-    if (!isEmpty(queue)) {
+void processPrintJob(PrinterQueue *queue) 
+{
+    if (!isEmpty(queue)) 
+    {
         PrintJob *jobToRemove = queue->front;
 
         printf("Printing: %s\n", jobToRemove->document);
@@ -62,29 +89,38 @@ void processPrintJob(PrinterQueue *queue) {
         queue->front = jobToRemove->next;
         free(jobToRemove);
 
-        if (queue->front == NULL) {
+        if (queue->front == NULL) 
+        {
             queue->rear = NULL;
         }
 
         queue->count--;
-    } else {
+    } 
+    else 
+    {
         printf("The printer queue is empty. No print jobs to process!\n");
     }
 }
 
-int main() {
+int main() 
+{
+    //initialize the printer queue
     PrinterQueue printerQueue;
     initializePrinterQueue(&printerQueue);
-
+    
     int choice;
     char *document = (char *)malloc(MAX_DOCUMENT_LENGTH * sizeof(char));
 
-    if (document == NULL) {
+    // Check if memory allocation failed
+    if (document == NULL) 
+    {
         printf("Memory allocation failed. Exiting.\n");
         return 1;  // Return a non-zero value to indicate an error
     }
-
-    while (1) {
+    
+    // Loop until the user chooses to exit
+    while (1) 
+    {
         printf("\n Printer Queue \n");
         printf("1. Submit a print job\n");
         printf("2. Process print job\n");
@@ -93,7 +129,8 @@ int main() {
         printf("Enter your choice (1/2/3): ");
         scanf("%d", &choice);
 
-        switch (choice) {
+        switch (choice) 
+        {
             case 1:
                 printf("Enter the document name: ");
                 scanf("%s", document);
